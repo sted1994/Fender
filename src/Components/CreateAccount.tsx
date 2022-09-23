ff3824965ae4439d5b4ad7df855dbb19474b8c64
Stephen Tedesco <s_tylert@icloud.com>
2022-09-23 11:26:58 -0600

    Create initial login component

---
+++ src/Components/CreateAccount.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const CreateAccount = () => {
+  return (
+    <section className="landingpage-section">
+      Create an Account
+    </section>
+  )
+}
---
+++ src/Components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../CSS/Header.css'
+
+
+export const Header = () => {
+  return (
+    <section className="header-section">
+      {/* <img className='header-background' src={require('../Images/fender_Cover.jpeg')}/> */}
+    </section>
+  )
+}
