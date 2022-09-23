739ec3c4a497781dd4c4b7e7e5f86c9948539c04
Stephen Tedesco <s_tylert@icloud.com>
2022-09-23 11:44:34 -0600

    Create initial component to create account

--- src/Components/CreateAccount.tsx
+++ src/Components/CreateAccount.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import '../CSS/CreateAccount.css'
 
 export const CreateAccount = () => {
   return (
     <section className="landingpage-section">
       Create an Account
+      <div className='create-account-background'>
+        Join The Comunity
+      </div>
+      <button>Create Free Account</button>
     </section>
   )
 }
